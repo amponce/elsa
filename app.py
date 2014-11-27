f6027820704d28087db7d39ee77fcb629ee32fa2
Warren Bartolome <warren.bartolome@gmail.com>
2014-11-27 00:44:10 -0800

    Added initial handling for resume submission.

--- app.py
+++ app.py
@@ -71,7 +71,20 @@ def home():
 	resume = db.session.query(models.Resume).filter_by(user_id=login.current_user).first()
 
 	return render_template('home.html', logged_in=login.current_user.is_authenticated()
-									  , resume=resume)
+						   , resume=resume)
+
+
+@app.route('/saveResume')
+def saveResume():
+	form = eforms.Resume(request.form)
+	resume = models.Resume()
+	form.populate_obj(resume)
+
+	db.session.add(resume)
+	db.session.commit()
+
+	flash('resume saved')
+	return redirect(url_for('home'))
 
 
 @app.route('/register', methods=['POST'])
@@ -103,64 +116,6 @@ def register():
 def logout():
     login.logout_user()
     return redirect(url_for('index'))
-
-
-@app.route('/one', methods=['GET'])
-def getStarted():
-	email = request.args.get('email')
-	first_search = request.args.get('first_search')
-	screen = 1
-
-	# update this to write to the paging metadata table
-	paging = request.args.get('paging')
-	if not paging:
-		paging = 0
-
-	data = indeed.getTree(first_search, paging * 5)
-	results = indeed.getResults(data)
-
-	user = models.User
-	skill = models.Skills
-	db = models.db
-	page = models.Pages
-
-	#next try to add the visitor to get an email
-	visitor = user.query.filter_by(email=email).first()
-	if visitor is None:
-		try:
-			visitor = user(email=email, created=None)
-			db.session.add(visitor)
-			db.session.commit()
-		except Exception as e:
-			db.session.rollback()
-
-	first_page = page.query.filter(page.user_id == visitor.id).filter(page.screen == 1).first()
-	try:
-		first_page = page(user_id=visitor.id, screen=screen, page=1, created=None, modified=None)
-		db.session.add(first_page)
-		db.session.commit()
-	except Exception as e:
-		db.session.rollback()
-
-	#finally, add the skill
-	first_skill = skill.query.filter(skill.user_id == visitor.id).filter(skill.skill_num == 1).first()
-	if first_skill is None:
-		try:
-			first_skill = skill(user_id=visitor.id, skill=first_search, skill_num=1, created=None)
-			db.session.add(first_skill)
-			db.session.commit()
-		except Exception as e:
-			db.session.rollback()
-	else:
-		first_skill.skill = first_search
-		db.session.commit()
-
-	return render_template('joblist.html', first_search=first_search,
-						   results=results,
-						   email=email,
-						   skill_check=0,
-						   visitor_id=visitor.id,
-						   screen=screen)
 
 
 if __name__ == '__main__':
