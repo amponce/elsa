0a681aca09f22d98beca95cc15aae0a66a62c01a
Warren Bartolome <warren.bartolome@gmail.com>
2014-12-12 21:23:48 -0800

    Added other functions to generate data.

--- make_data.py
+++ make_data.py
@@ -22,6 +22,15 @@ def create_users(n):
         try:
             db.session.add(user)
             db.session.commit()
+
+            resume = generateResume(user.id)
+            test_id = makeABTest(user.id)
+
+            makeRecipes(test_id, resume)
+
+            if user.role_id == 1:
+                makeJobPosting(user.id)
+
             s += 1
         except Exception as e:
             e += 1
@@ -30,3 +39,64 @@ def create_users(n):
     return msg
 
 #presuming there's more than 15 records existing
+def generateResume(user_id):
+    text = fake.text(300) + '\n' + fake.text(300) + '\n' + fake.text(300)
+    resume = models.Resume()
+    resume.user_id = user_id
+    resume.resume = text
+
+    try:
+        db.session.add(resume)
+        db.session.commit()
+    except:
+        db.session.rollback()
+
+    return text
+
+def makeABTest(user_id):
+    test_name = fake.text(100)
+    hypothesis = fake.text(100)
+
+    test = models.ABTests()
+    test.test_name = test_name
+    test.hypothesis = hypothesis
+    test.user_id = user_id
+
+    try:
+        db.session.add(test)
+        db.session.commit()
+    except:
+        db.session.rollback()
+
+    return test.id
+
+def makeRecipes(test_id, resume):
+    for x in range(1, 3):
+        test_recipe = 'Control' if x == 1 else 'Recipe B'
+        recipes = models.Recipes()
+        recipe.test_id = test_id
+        recipes.recipe = test_recipe
+
+        recipe_b = fake.text(300) + '\n' + fake.text(300) + '\n' + fake.text(300)
+        version = resume if test_recipe == 'Control' else recipe_b
+        recipes.version = version
+
+        try:
+            db.session.add(recipes)
+            db.session.commit()
+        except:
+            db.session.rollback()
+
+def makeJobPosting(poster_id):
+    job = models.Jobs()
+    job.title = fake.text(50)
+    job.description = fake.text(500)
+    job.skills = fake.text(50)
+    job.url = fake.url()
+    job.poster_id = poster_id
+
+    try:
+        db.session.add(job)
+        db.session.commit()
+    except:
+        db.session.rollback()
